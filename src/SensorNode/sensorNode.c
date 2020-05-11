bbaf02170c19afbc1926bed255613cb9436c89a3
Detobel36 <detobel36@phoenix-rebirth.fr>
2020-05-11 18:47:10 +0200

    [WIP] Update to have one channel per communication

--- src/SensorNode/sensorNode.c
+++ src/SensorNode/sensorNode.c
@@ -20,11 +20,16 @@
 #define BROADCAST_DELAY 15
 #define MAX_RANK 255
 
+#define BROADCAST_CHANNEL 129
+#define RUNICAST_CHANNEL_DATA 144
+#define RUNICAST_CHANNEL_BROADCAST 146
+
 
 /*---------------------------------------------------------------------------*/
 // VARIABLES
 static struct broadcast_conn broadcast;
-static struct runicast_conn runicast;
+static struct runicast_conn runicast_data;
+static struct runicast_conn runicast_broadcast;
 static uint8_t rank = MAX_RANK;
 static linkaddr_t parent_addr;
 static uint16_t parent_last_rssi;
@@ -41,136 +46,105 @@ AUTOSTART_PROCESSES(&broadcast_process, &runicast_process, &data_process);
 
 
 /*---------------------------------------------------------------------------*/
-// BROADCAST
+// Receive broadcast
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  struct general_packet *packet;
-  packet = packetbuf_dataptr();
+  printf("broadcast message received from %d.%d.\n",
+         from->u8[0], from->u8[1]);
 
-  printf("broadcast message received from %d.%d. Type: %d and rank: %d\n",
-         from->u8[0], from->u8[1], packet->type, packet->rank);
+  // Respond to broadcast
+  if(rank != MAX_RANK) {
+    struct rank_packet packet_response;
+    packet_response.rank = rank;
 
-  // Request to find parent
-  if(packet->type == DISCOVERY_REQ) {
+    while(runicast_is_transmitting(&runicast_broadcast)) { }
+    packetbuf_clear();
 
-    // Respond to broadcast
-    if(rank != MAX_RANK) {
-      struct general_packet packet_response;
-      packet_response.type = DISCOVERY_RESP;
-      packet_response.rank = rank;
-
-      int countTransmission=0;
-      while (runicast_is_transmitting(&runicast) && ++countTransmission < MAX_RETRANSMISSIONS) {}
-
-      packetbuf_copyfrom(&packet_response, sizeof(struct general_packet));
-      printf("Send response to broadcast\n");
-      runicast_send(&runicast, from, MAX_RETRANSMISSIONS);
-    }
-
-  } else {
-    printf("Unknow packet broadcsat: %d\n", packet->type);
+    packetbuf_copyfrom(&packet_response, sizeof(struct rank_packet));
+    printf("Send response to broadcast\n");
+    runicast_send(&runicast_broadcast, from, MAX_RETRANSMISSIONS);
+    packetbuf_clear();
   }
-}
-
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
-PROCESS_THREAD(broadcast_process, ev, data)
-{
-  static struct etimer et;
-
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
-
-  PROCESS_BEGIN();
-
-  broadcast_open(&broadcast, 129, &broadcast_call);
-
-  while(1) {
-
-    struct general_packet packet;
-    packet.type = DISCOVERY_REQ;
-    packet.rank = rank;
-    packetbuf_copyfrom(&packet, sizeof(struct general_packet));
-    broadcast_send(&broadcast);
-    printf("broadcast message sent\n");
-
-    /* Delay between BROADCAST_DELAY and 2*BROADCAST_DELAY seconds */
-    // TODO may be add more time
-    etimer_set(&et, CLOCK_SECOND * BROADCAST_DELAY + (random_rand() % (CLOCK_SECOND * BROADCAST_DELAY)));
-
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-  }
-
-  PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
 
-
 /*---------------------------------------------------------------------------*/
-// RUNICAST
-
+// Receive broadcast response
 static void
-recv_runicast(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
+recv_broadcast_runicast(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
 {
-  struct abstract_packet *abstr_packet;
-  abstr_packet = packetbuf_dataptr();
+  struct rank_packet *rank_packet = packetbuf_dataptr();
+  uint16_t getting_rank = rank_packet->rank;
+  printf("Discovery response from %d.%d, seqno %d, rank: %d\n", from->u8[0], from->u8[1], seqno,
+    getting_rank);
+
   uint16_t current_rssi = packetbuf_attr(PACKETBUF_ATTR_RSSI);
 
-  // TODO
-  // Update rssi when receive packet from parent
-  // parent_last_rssi = current_rssi;
+  // If no rank
+  if(rank == MAX_RANK) {
+    rank = getting_rank+1;
+    parent_addr = *from;
+    parent_last_rssi = current_rssi;
+    printf("Init rank: %d (quality: %d)\n", rank, current_rssi);
 
-  // On other node reply
-  if (abstr_packet->type == DISCOVERY_RESP) {
+  // If broadcast from parent
+  } else if(parent_addr.u8[0] == from->u8[0] && parent_addr.u8[1] == from->u8[1]) {
+    parent_last_rssi = current_rssi;
+    printf("Update parent quality signal (new: %d)\n", current_rssi);
 
-    struct general_packet *general_packet = (struct general_packet *) abstr_packet;
-    uint16_t getting_rank = general_packet->rank;
-    printf("Discovery response from %d.%d, seqno %d, rank: %d\n", from->u8[0], from->u8[1], seqno,
-      getting_rank);
-
-    if(rank == MAX_RANK) {
+  // If potential new parent
+  } else if(rank > getting_rank) {  // current_rank > new_getting_rank
+    if(current_rssi > parent_last_rssi) {
       rank = getting_rank+1;
       parent_addr = *from;
       parent_last_rssi = current_rssi;
-      printf("Init rank: %d (quality: %d)\n", rank, current_rssi);
-
-    } else if(rank > getting_rank+1) {
-      if(current_rssi > parent_last_rssi) {
-        rank = getting_rank+1;
-        parent_addr = *from;
-        parent_last_rssi = current_rssi;
-        printf("Change parent and get rank: %d (quality signal: %d)\n", rank, parent_last_rssi);
-      } else {
-        printf("Quality signal not enought to change parent (new: %d, parent: %d)\n", current_rssi, parent_last_rssi);
-      }
-
-    } else if(parent_addr.u8[0] == from->u8[0] && parent_addr.u8[1] == from->u8[1]) {
-      parent_last_rssi = current_rssi;
-      printf("Update parent quality signal (new: %d)\n", current_rssi);
+      printf("Change parent and get rank: %d (quality signal: %d)\n", rank, parent_last_rssi);
+    } else {
+      printf("Quality signal not enought to change parent (new: %d, parent: %d)\n", current_rssi, parent_last_rssi);
     }
-    // TODO check "if(rank == getting_rank+1)" and better signal quality !
 
-
-  // When we receive packet
-  } else if(abstr_packet->type == PACKET_DATA) {
-
-    struct data_packet *forward_data_packet = (struct data_packet *) abstr_packet;
-
-    linkaddr_t source_addr = forward_data_packet->address;
-    printf("Forward data %d (source: %d.%d) from %d.%d to %d.%d, seqno %d\n", forward_data_packet->data, 
-      source_addr.u8[0], source_addr.u8[1], from->u8[0], from->u8[1], parent_addr.u8[0], 
-      parent_addr.u8[1], seqno);
-    packetbuf_copyfrom(forward_data_packet, sizeof(struct data_packet));
-  
-    int countTransmission = 0;
-    while (runicast_is_transmitting(&runicast) && ++countTransmission < MAX_RETRANSMISSIONS) {}
-    runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
-
-
-  // Unknow packet
-  } else {
-    printf("[UNKNOW] runicast message received from %d.%d, seqno %d\n",
-        from->u8[0], from->u8[1], seqno);
   }
+}
+
+static void
+sent_broadcast_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast broadcast message sent to %d.%d, retransmissions %d\n",
+   to->u8[0], to->u8[1], retransmissions);
+}
+
+static void
+timedout_broadcast_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+{
+  printf("runicast broadcast message timed out when sending to %d.%d, retransmissions %d\n",
+   to->u8[0], to->u8[1], retransmissions);
+}
+
+/*---------------------------------------------------------------------------*/
+
+
+/*---------------------------------------------------------------------------*/
+// Receive data packet
+static void
+recv_data_runicast(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
+{
+  struct abstract_packet *abstr_packet;
+  abstr_packet = packetbuf_dataptr();
+
+  struct data_packet *forward_data_packet = (struct data_packet *) abstr_packet;
+
+  while(runicast_is_transmitting(&runicast_broadcast)) { }
+  packetbuf_clear();
+  
+  linkaddr_t source_addr = forward_data_packet->address;
+  printf("Forward data %d (source: %d.%d) from %d.%d to %d.%d, seqno %d\n", forward_data_packet->data, 
+    source_addr.u8[0], source_addr.u8[1], from->u8[0], from->u8[1], parent_addr.u8[0], 
+    parent_addr.u8[1], seqno);
+  packetbuf_copyfrom(forward_data_packet, sizeof(struct data_packet));
+
+  runicast_send(&runicast_data, &parent_addr, MAX_RETRANSMISSIONS);
+  packetbuf_clear();
 
   /* OPTIONAL: Sender history */
   // struct history_entry *e = NULL;
@@ -198,34 +172,83 @@ recv_runicast(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
   //   /* Update existing history entry */
   //   e->seq = seqno;
   // }
-
 }
 
 static void
-sent_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+sent_data_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
 {
-  printf("runicast message sent to %d.%d, retransmissions %d\n",
+  printf("runicast data message sent to %d.%d, retransmissions %d\n",
    to->u8[0], to->u8[1], retransmissions);
 }
 
 static void
-timedout_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
+timedout_data_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
 {
-  printf("runicast message timed out when sending to %d.%d, retransmissions %d\n",
+  printf("runicast data message timed out when sending to %d.%d, retransmissions %d\n",
    to->u8[0], to->u8[1], retransmissions);
 }
 
-static const struct runicast_callbacks runicast_callbacks = {
-                   recv_runicast,
-                   sent_runicast,
-                   timedout_runicast};
-PROCESS_THREAD(runicast_process, ev, data)
+/*---------------------------------------------------------------------------*/
+
+
+
+static const struct runicast_callbacks runicast_data_callbacks = {
+                   recv_data_runicast,
+                   sent_data_runicast,
+                   timedout_data_runicast};
+
+static const struct runicast_callbacks runicast_broadcast_callbacks = {
+                   recv_broadcast_runicast,
+                   sent_broadcast_runicast,
+                   timedout_broadcast_runicast};
+
+
+
+
+/*================================ THREADS ==================================*/
+
+/*---------------------------------------------------------------------------*/
+// Send broadcast threat
+static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
+PROCESS_THREAD(broadcast_process, ev, data)
 {
-  PROCESS_EXITHANDLER(runicast_close(&runicast);)
+  static struct etimer et;
+
+  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
   PROCESS_BEGIN();
 
-  runicast_open(&runicast, 144, &runicast_callbacks);
+  broadcast_open(&broadcast, BROADCAST_CHANNEL, &broadcast_call);
+
+  while(1) {
+
+    // struct general_packet packet;
+    // packet.type = DISCOVERY_REQ;
+    // packet.rank = rank;
+    // packetbuf_copyfrom(&packet, sizeof(struct general_packet));
+    broadcast_send(&broadcast);
+    // printf("broadcast message sent\n");
+
+    /* Delay between BROADCAST_DELAY and 2*BROADCAST_DELAY seconds */
+    // TODO may be add more time
+    etimer_set(&et, CLOCK_SECOND * BROADCAST_DELAY + (random_rand() % (CLOCK_SECOND * BROADCAST_DELAY)));
+
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+// TODO
+PROCESS_THREAD(runicast_process, ev, data)
+{
+  PROCESS_EXITHANDLER(runicast_close(&runicast_broadcast);)
+
+  PROCESS_BEGIN();
+
+  runicast_open(&runicast_broadcast, RUNICAST_CHANNEL_BROADCAST, &runicast_broadcast_callbacks);
 
   /* OPTIONAL: Sender history */
   // list_init(history_table);
@@ -263,11 +286,15 @@ PROCESS_THREAD(runicast_process, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
 // DATA PROCESS
 PROCESS_THREAD(data_process, ev, data)
 {
-  PROCESS_EXITHANDLER(runicast_close(&runicast);)
+  PROCESS_EXITHANDLER(runicast_close(&runicast_data);)
   PROCESS_BEGIN();
+
+  runicast_open(&runicast_data, RUNICAST_CHANNEL_DATA, &runicast_data_callbacks);
 
   while(1) {
     static struct etimer et;
@@ -279,6 +306,9 @@ PROCESS_THREAD(data_process, ev, data)
       // Generates a random int between 0 and 100
       uint8_t random_int = random_rand() % (100 + 1 - 0) + 0; // (0 for completeness)
 
+      while(runicast_is_transmitting(&runicast_data)) { }
+      packetbuf_clear();
+
       struct data_packet packet;
       packet.type = PACKET_DATA;
       packet.data = random_int;
@@ -287,12 +317,20 @@ PROCESS_THREAD(data_process, ev, data)
       packetbuf_copyfrom(&packet, sizeof(struct data_packet));
       
       int countTransmission = 0;
-      while (runicast_is_transmitting(&runicast) && ++countTransmission < MAX_RETRANSMISSIONS) {}
-      runicast_send(&runicast, &parent_addr, MAX_RETRANSMISSIONS);
+      // while (runicast_is_transmitting(&runicast) && ++countTransmission < MAX_RETRANSMISSIONS) {}
+      while (runicast_is_transmitting(&runicast)) {
+        ++countTransmission;
+        printf("Wait runicast transmit %d\n", countTransmission);
+      }
+      runicast_send(&runicast_data, &parent_addr, MAX_RETRANSMISSIONS);
       printf("Send random data %d\n", random_int);
+      packetbuf_clear();
     }
     // SEND DATA
   }
 
   PROCESS_END();
 }
+/*---------------------------------------------------------------------------*/
+
+/*===========================================================================*/
