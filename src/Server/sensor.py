835e47975eeee25d8c58b263689b79624dc34fa8
Detobel36 <detobel36@phoenix-rebirth.fr>
2020-05-15 17:04:56 +0200

    Add computation of least square root

--- src/Server/sensor.py
+++ src/Server/sensor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
+import numpy as np
+
 DEBUG = True
+NUMBER_VALUE_TO_EVAL = 30
+THRESHOLD = -5
+
 
 """
 TODO:
@@ -22,17 +27,23 @@ class Sensor:
                 "(total " + str(len(self.listValues)) + ")")
 
     def _computeLeastSquareRoot(self):
-        # TODO
-        return False
+        if(len(self.listValues) < NUMBER_VALUE_TO_EVAL):
+            print("[DEBUG] (" + str(self.address) + ") No enought value for valve " + \
+                "(total " + str(len(self.listValues)) + ")")
+            return None
+
+        x = np.array([i for i in range(NUMBER_VALUE_TO_EVAL)])
+        y = np.array(self.listValues[len(self.listValues)-NUMBER_VALUE_TO_EVAL:])
+        A = np.vstack([x, np.ones(len(x))]).T
+        m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+
+        return m < THRESHOLD
+
 
     # TODO update this name
     def getOpenValve(self):
-        if(len(self.listValues) >= 30):
-            openValve = self._computeLeastSquareRoot()
-            if(DEBUG):
-                print("[DEBUG] (" + str(self.address) + ") Need open valve: " + str(openValve))
-            return openValve
+        openValve = self._computeLeastSquareRoot()
         if(DEBUG):
-            print("[DEBUG] (" + str(self.address) + ") No enought value for valve " + \
-                "(total " + str(len(self.listValues)) + ")")
-        return None
+            print("[DEBUG] (" + str(self.address) + ") Need open valve: " + str(openValve))
+
+        return openValve
